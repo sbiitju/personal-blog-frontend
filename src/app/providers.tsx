f6d642ea651690a3476f9f9ef1da031a32fc69e9
Hosain <cryptichasan925@gmail.com>
2025-03-17 23:08:07 +0600

    user provier done

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import UserProvider from "@/context/user.provider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactNode, useState } from "react";
 
@@ -7,7 +8,9 @@ const Providers = ({ children }: { children: ReactNode }) => {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <UserProvider>{children}</UserProvider>
+    </QueryClientProvider>
   );
 };
 
