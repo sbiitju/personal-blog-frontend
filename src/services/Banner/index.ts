3163f15101d08fb13eb20bf298ae4c2062b3e2f8
Hosain <cryptichasan925@gmail.com>
2025-03-17 23:29:19 +0600

    create delete and get all banner servcies

---
+++ src/services/Banner/index.ts
@@ -0,0 +1,35 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use server";
+
+import axiosInstance from "@/lib/AxiousInstance";
+import { FieldValues } from "react-hook-form";
+
+export const getAllBanner = async (domain: string) => {
+  let fetchOptions = {};
+  fetchOptions = {
+    cache: "no-store",
+  };
+  const { data } = await axiosInstance.get(`/banner/${domain}`, fetchOptions);
+  return data;
+};
+
+export const createBanner = async (bannerData: FieldValues) => {
+  try {
+    const { data } = await axiosInstance.post(
+      "/banner/create-banner",
+      bannerData
+    );
+    return data;
+  } catch (error: any) {
+    const data = {
+      success: false,
+      message: error?.response?.data?.message,
+    };
+    return data;
+  }
+};
+
+export const deleteBanner = async (id: string) => {
+  const { data } = await axiosInstance.delete(`/banner/${id}`);
+  return data;
+};
