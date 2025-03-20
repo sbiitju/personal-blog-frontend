f6ec7d3b216a5f2efb3d946cd43c6bca872483ec
Hosain <cryptichasan925@gmail.com>
2025-03-20 22:20:57 +0600

    page details page done

--- src/app/(withCommonLayout)/(home)/page.tsx
+++ src/app/(withCommonLayout)/(home)/page.tsx
@@ -9,8 +9,6 @@ import React, { useEffect, useState } from "react";
 
 const Home = () => {
   const [domain, setDomain] = useState<string>("");
-
- 
   useEffect(() => {
     if (typeof window !== "undefined") {
       setDomain(window.location.hostname);
