acf749b831d4740ac9b66f2bf96aae4d18af10b4
Hosain <cryptichasan925@gmail.com>
2025-03-23 23:30:12 +0600

    user routes protection

---
+++ src/middleware.ts
@@ -0,0 +1,41 @@
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+import { getCurrentUser } from "./services/Auth";
+
+// Define role-based routes
+const roleBasedRoutes = {
+  technical: [/^\/technical/],
+  admin: [/^\/admin/],
+};
+
+type Role = keyof typeof roleBasedRoutes;
+
+export async function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+  const user = await getCurrentUser();
+ 
+  if (!user && (pathname.startsWith("/admin") || pathname.startsWith("/technical"))) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+ 
+  if (user && (pathname.startsWith("/admin") || pathname.startsWith("/technical"))) {
+    if (user?.role) {
+      const allowedRoutes = roleBasedRoutes[user?.role as Role];
+
+      // Allow access if user's role matches
+      if (allowedRoutes && allowedRoutes.some((route) => pathname.match(route))) {
+        return NextResponse.next();
+      }
+    }
+ 
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  // ✅ Allow access to all other routes
+  return NextResponse.next();
+}
+
+// Configuration for matching paths
+export const config = {
+  matcher: ["/admin/:path*", "/technical/:path*"],
+};
