5f6046d55f1def32c930cdd7e538ce0548920315
Hosain <cryptichasan925@gmail.com>
2025-04-20 23:47:01 +0600

    footer updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Providers from "./providers"; 
+import Providers from "./providers";
+import { headers } from "next/headers";
+import envConfig from "@/config/env.confg";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -13,10 +15,22 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
-  title: "Personal Blog",
-  description: "This is Personal blog app",
-};
+// ✅ Only this async metadata export now
+export async function generateMetadata(): Promise<Metadata> {
+  const headersList = await headers();
+  const domain = headersList.get("host") || "default-domain.com";
+ 
+
+  const res = await fetch(`${envConfig.baseApi}/client/${domain}`, {
+    cache: "no-store",
+  });
+  const data = await res.json();
+
+  return {
+    title: data?.data?.name || "A Place to Share My Work and Story",
+    description: data?.data?.bio || "Highlight my skills, experiences, and creative projects with a personal portfolio website.",
+  };
+}
 
 export default function RootLayout({
   children,
